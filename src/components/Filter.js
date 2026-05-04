1180528f7426a528c0f129540ae0da4361434360
aperin17 <perinandjela5@gmail.com>
2026-05-04 16:33:32 +0200

    Sredjeno filtriranje

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -8,35 +8,45 @@ import Typography from '@mui/material/Typography';
 
 export default function Filter({ apartments, setFilteredApartments }) {
 
-    // const { apartments, setFilteredApartments } = useStore()
-    const filterList = useStore((state) => state.filterList)
     const setFilterList = useStore((state) => state.setFilterList);
 
-    const filterApartments = () => {
-        return setFilteredApartments(apartments.filter(apartment => (
-            apartment.city?.toLowerCase().includes(filterList.city.toLowerCase())
+
+    const filterApartments = (e) => {
+        e.preventDefault();
+
+        const formData = new FormData(e.currentTarget);
+
+        const filters = {
+            city: formData.get("city") || "",
+            maxPrice: formData.get("maxPrice") || "",
+            guests: formData.get("guests") || ""
+        };
+
+        setFilterList(filters);
+
+        setFilteredApartments(apartments.filter(apartment => (
+            apartment.city?.toLowerCase().includes(filters.city.toLowerCase())
             &&
-            Number(apartment.pricePerNight) <= (filterList.maxPrice === "" ? Infinity : Number(filterList.maxPrice))
+            Number(apartment.pricePerNight) <= (filters.maxPrice === "" ? Infinity : Number(filters.maxPrice))
             &&
-            Number(apartment.guests) >= (filterList.guests === "" ? 1 : Number(filterList.guests))
+            Number(apartment.guests) >= (filters.guests === "" ? 1 : Number(filters.guests))
         )))
-    }
+    };
+
 
     return (
         <div className="filter-container">
             <Typography variant="h5" component="div" sx={{ fontSize: "20px", marginBottom: "12px" }}>
                 Filter apartments
             </Typography>
-            <Box component="form" className="filter-form" noValidate autoComplete="off" >
-                <TextField className="filter-input" label="City" fullWidth id="outlined-size-small" size="small" variant="outlined"
-                    onChange={(e) => { setFilterList({ city: e.target.value }) }} />
-                <TextField className="filter-input" label="Maximum price per night (EUR)" fullWidth id="outlined-size-small" size="small" variant="outlined"
-                    onChange={(e) => { setFilterList({ maxPrice: e.target.value }) }} />
-                <TextField className="filter-input" label="Number of guests" fullWidth id="outlined-size-small" size="small" variant="outlined"
-                    onChange={(e) => { setFilterList({ guests: e.target.value }) }} />
+            <Box component="form" className="filter-form" onSubmit={filterApartments} noValidate autoComplete="off" >
 
-                <Button className="app-button" variant="contained" size="small"
-                    onClick={filterApartments}>Filter</Button>
+                <TextField className="filter-input" name="city" label="City" fullWidth id="outlined-size-small" size="small" variant="outlined" />
+                <TextField className="filter-input" name="maxPrice" label="Maximum price per night (EUR)" fullWidth id="outlined-size-small" size="small" variant="outlined" />
+                <TextField className="filter-input" name="guests" label="Number of guests" fullWidth id="outlined-size-small" size="small" variant="outlined" />
+
+                <Button type="submit" className="app-button" variant="contained" size="small">Filter</Button>
+
             </Box>
         </div >
     );
